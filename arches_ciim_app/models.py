71ab50d5e4607503ff8d5b40ac773ba8d844ebbc
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2024-11-18 07:16:27 +0000

    Receiver to populate lre table on editlog save

--- arches_ciim_app/models.py
+++ arches_ciim_app/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_delete, pre_save, post_save
 
 from arches.app.models.resource import Resource
 from arches.app.models.tile import Tile
+from arches.app.models.models import EditLog
 
 import uuid
 
@@ -16,6 +17,20 @@ class LatestResourceEdit(models.Model):
     username = models.TextField(blank=True, null=True)
     userid = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(blank=True, null=True)
+
+    @receiver(post_save, sender=EditLog)
+    def update_latest_resource_edit(instance, **kwargs):
+        if LatestResourceEdit.objects.filter(resourceinstanceid=instance.resourceinstanceid).exists():
+            LatestResourceEdit.objects.get(resourceinstanceid=instance.resourceinstanceid).delete()
+        latest_edit = LatestResourceEdit()
+        latest_edit.resourceinstanceid = instance.resourceinstanceid
+        latest_edit.resourcedisplayname = instance.resourcedisplayname
+        latest_edit.edittype = instance.edittype
+        latest_edit.graphid = instance.resourceclassid
+        latest_edit.userid = instance.userid
+        latest_edit.username = instance.user_username
+        latest_edit.timestamp = instance.timestamp
+        latest_edit.save()
 
     class Meta:
         managed = True
